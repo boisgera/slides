9d7c28dd0059c2a80b0634d78d3771472b2941a9
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-01-10 18:18:39 +0100

    Update dependencies and remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import civet from '@danielx/civet/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     civet({ts: "preserve"}), 
